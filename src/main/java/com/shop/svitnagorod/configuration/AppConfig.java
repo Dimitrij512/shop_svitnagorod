ada4297c53452d62abc12fd640189d9637fc07a9
Dimitrij512 <hfl-if@ukr.net>
2017-01-10 10:54:31 +0200

    changed structure

--- src/main/java/com/shop/svitnagorod/configuration/AppConfig.java
+++ src/main/java/com/shop/svitnagorod/configuration/AppConfig.java
@@ -25,7 +25,8 @@ public class AppConfig extends WebMvcConfigurerAdapter {
   @Bean
   public TilesConfigurer tilesConfigurer() {
     TilesConfigurer tilesConfigurer = new TilesConfigurer();
-    tilesConfigurer.setDefinitions(new String[] { "/WEB-INF/views/**/tiles.xml" });
+    tilesConfigurer.setDefinitions(new String[] { "/WEB-INF/views/**/tiles.xml", "/WEB-INF/views/**/guest.xml",
+        "/WEB-INF/views/**/user.xml", "/WEB-INF/views/**/admin.xml" });
     tilesConfigurer.setCheckRefresh(true);
     return tilesConfigurer;
   }
